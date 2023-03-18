067f0b13f538a023e2a7d3bc1ddea31d8b8082d1
kelvin901 <wawerukelvin2019@gmail.com>
2023-03-18 22:25:41 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const result = () => 2000 / 1000;
+console.log(result()); // Output: 2
+
+const square = (x) => x * x;
+console.log(square(2)); // Output: 4
+
+const add = (a, b) => a + b;
+
+// Call the add function with a=3 and b=4
+const addition = add(3, 4);
+
+console.log(addition); // Output: 7
